2f3f9934b5b12db9e1ee70a30856754b4199d143
b3h3rkz <barjebernard@gmail.com>
2016-03-21 07:58:10 +0000

    added cors header to allow request from all origins

--- NoticeBoard/settings.py
+++ NoticeBoard/settings.py
@@ -36,12 +36,14 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_markdown',
     'Noticeboard',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -51,6 +53,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
+CORS_ORIGIN_ALLOW_ALL = True
 MARKDOWN_EDITOR_SKIN = 'simple'
 
 ROOT_URLCONF = 'NoticeBoard.urls'
